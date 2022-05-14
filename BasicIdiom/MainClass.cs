7068b49fd63b88b041e5ee9ee399c34d2aab96ab
morimori-coder <67218438+morimori-coder@users.noreply.github.com>
2022-05-14 09:13:10 +0900

    基本イディオムの演習問題解答

--- BasicIdiom/MainClass.cs
+++ BasicIdiom/MainClass.cs
@@ -10,7 +10,37 @@ namespace BasicIdiom
     {
         public static void Main() 
         {
-            var yearMonth = new YearMonth(2022,2);
+            YearMonth[] yearMonths = new YearMonth[5];
+            yearMonths[0] = new YearMonth(1001, 1);
+            yearMonths[1] = new YearMonth(1002, 2);
+            yearMonths[2] = new YearMonth(1003, 3);
+            yearMonths[3] = new YearMonth(1004, 4);
+            yearMonths[4] = new YearMonth(1005, 12);
+
+            yearMonths.ToList().ForEach(n => Console.WriteLine(n));
+
+            var firstYearMonth = GetFirst21CenturyInArray(yearMonths);
+            var firstYear = firstYearMonth == null ? "21世紀のデータはありません。" : $"{firstYearMonth.Year.ToString()}年";
+            Console.WriteLine(firstYear);
+
+            var newYearMonths = yearMonths.OrderBy(y => y.Year).Select(y => y.AddOneMonth(y.Year, y.Month)).ToList();
+            newYearMonths.ForEach(n => Console.WriteLine(n));
+
+            Console.ReadKey();
+        }
+
+        static private YearMonth GetFirst21CenturyInArray(IEnumerable<YearMonth> yearMonths)
+        {
+            YearMonth result = null;
+            foreach (var y in yearMonths)
+            {
+                if (y.Is21Century)
+                {
+                    result = y;
+                    break;
+                }
+            }
+            return result;
         }
 
         static void CallSwap()
