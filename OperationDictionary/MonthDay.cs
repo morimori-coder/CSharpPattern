ae6b0c8871d8bb4588ec5ee99c82cf85d94ed5fc
morimori-coder <67218438+morimori-coder@users.noreply.github.com>
2022-05-18 21:49:47 +0900

    7章の章末問題とサンプルコード

---
+++ OperationDictionary/MonthDay.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationDictionary
+{
+    internal class MonthDay
+    {
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public MonthDay(int month, int day)
+        {
+            this.Month = month; this.Day = day;
+            this.Day = day;
+        }
+
+        // MonthDay どうし の 比較 を する
+        public override bool Equals( object obj) 
+        {
+            var other = obj as MonthDay;
+            if (other == null) 
+                throw new ArgumentException();
+            return this.Day == other.Day && this.Month == other.Month;
+        }
+
+        public override int GetHashCode()
+        {
+            return Month.GetHashCode()*31 * Day.GetHashCode();
+        }
+    }
+}
---
+++ OperationDictionary/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace OperationDictionary
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            var sampleStr = "Cozy lummox gives smart squid who asks for job pen";
+
+
+            var dict = new Dictionary<MonthDay, string>
+            {
+                {new MonthDay(3,5), "珊瑚の日"},
+                { new MonthDay(8,4), "端の日"},
+                {new MonthDay(10,3), "登山の日"},
+            };
+
+            var md = new MonthDay(8, 4);
+            var s = dict[md];
+            Console.WriteLine(s);
+
+        }
+
+        private static void Exercise7_1_1(string sample)
+        {
+            //var countByAlphabet 
+            var dic = new Dictionary<char, int>();
+            foreach (var singleChar in sample)
+            {
+                var uc = char.ToUpper(singleChar);
+                if ('A' <= uc && uc <= 'Z') 
+                {
+                    if (dic.ContainsKey(uc))
+                        dic[uc]++;
+                    else
+                        dic[uc] = 1;
+                }
+            }
+            foreach (var item in dic.OrderBy(x => x.Key))
+                Console.WriteLine("{0}:{1}", item.Key, item.Value);
+        }
+    }
+}
