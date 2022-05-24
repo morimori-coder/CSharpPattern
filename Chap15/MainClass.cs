a7a7d63034ca11a8ab27bb959a61bc594025bdc9
morimori-coder <67218438+morimori-coder@users.noreply.github.com>
2022-05-25 07:17:43 +0900

    15章

---
+++ Chap15/MainClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap15
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var groups = Library.Categories
+                .GroupJoin(Library.Books,
+                    c => c.Id,
+                    b => b.CategoryId,
+                    (c, books) => new { Category = c.Name, Books = books });
+            foreach (var group in groups)
+            {
+                Console.WriteLine(group.Category);
+                foreach (var book in group.Books)
+                {
+                    Console.WriteLine($" {book.Title} ({book.PublishedYear} 年)");
+                }
+            }
+        }
+    }
+}
